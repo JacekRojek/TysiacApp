bf1e78417115fb30928d5c4a1b495d88f3fdd2e7
Jacek Rojek <j.rojek19@gmail.com>
2017-02-19 18:28:58 +0000

    added option to choose player

--- App/Components/PlayerRow.js
+++ App/Components/PlayerRow.js
@@ -6,20 +6,42 @@ import styles from './Styles/PlayerRowStyle'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
 import PlayersActions from '../Redux/PlayersRedux'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class PlayerRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.player = props.players.find(o => o.id === props.id)
+    this.state = {selected: this.player.selected};
+  }
+
   _handlePlayerSelected = () => {
     this.props.selectPlayer(this.props.id)
     NavigationActions.playerInfo()
   }
 
+  _handlePlayerChoosen = () => {
+    this.props.choosePlayer(this.props.id)
+  }
+
+  componentWillReceiveProps (newProps) {
+    this.setState({selected: newProps.players.find(o => o.id === newProps.id).selected})
+    // this.forceUpdate()
+  }
+
   render () {
     props = this.props
+    const iconColor = this.state.selected === true ? "green" : "#900"
     return (
-      <TouchableOpacity  style={styles.row} onPress={this._handlePlayerSelected}>
-        <Text style={styles.boldLabel}>{props.title}</Text>
-        <Text style={styles.label}>{'Total wins: '+props.description}</Text>
-      </TouchableOpacity >
+      <View style={styles.row}>
+        <TouchableOpacity  style={styles.textContainer} onPress={this._handlePlayerSelected}>
+          <Text style={styles.boldLabel}>{props.title}</Text>
+          <Text style={styles.label}>{'Total wins: '+props.description}</Text>
+        </TouchableOpacity >
+        <TouchableOpacity style={styles.iconContainer} onPress={this._handlePlayerChoosen}>
+          <Icon name="check" size={30} color = {iconColor} />
+        </TouchableOpacity >
+      </View>
     )
   }
 }
@@ -35,12 +57,15 @@ class PlayerRow extends React.Component {
 //   someSetting: false
 // }
 const mapStateToProps = (state) => {
-  return { }
+  return { 
+    players: state.players.players,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-     selectPlayer: (id) => dispatch(PlayersActions.selectPlayer(id))
+     selectPlayer: (id) => dispatch(PlayersActions.selectPlayer(id)),
+     choosePlayer: (id) => dispatch(PlayersActions.choosePlayer(id)),
   }
 }
 
